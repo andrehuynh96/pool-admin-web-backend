11e5726a850ac49ba6019898ec6273c1a6913590
Unknown <huyht@blockchainlabs.asia>
2020-02-27 13:51:30 +0700

    seed data
    seed data

---
+++ app/model/staking/seed/user-role.js
@@ -0,0 +1,32 @@
+const Role = require("app/model/staking").roles;
+const User = require("app/model/staking").users;
+const UserRole = require("app/model/staking").user_roles;
+
+(async () => {
+  let adminRole = await Role.findOne({
+    where: {
+      name: "Admin"
+    }
+  });
+  let user = await User.findOne({
+    where: {
+      email: "example@gmail.com"
+    }
+  });
+
+  if (user && adminRole) {
+    let roleUser = await UserRole.findOne({
+      where: {
+        user_id: user.id,
+        role_id: adminRole.id,
+      }
+    });
+
+    if (!roleUser) {
+      await UserRole.create({
+        user_id: user.id,
+        role_id: adminRole.id,
+      })
+    }
+  }
+})();
