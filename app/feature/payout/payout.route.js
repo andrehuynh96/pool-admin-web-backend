a173858ceb16c1173017958a475c31e64db6a0ee
Huynh Ngoc My <myhn@blockchainlabs.asia>
2020-03-09 17:47:04 +0700

    Update permission key and swagger get list payout config

--- app/feature/payout/payout.route.js
+++ app/feature/payout/payout.route.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get(
   '/payouts',
   authenticate,
-  authority(Permission.VIEW_LIST_PAYOUT_STAKING_PLATFORM),
+  authority(Permission.VIEW_LIST_PAYOUT_CONFIG),
   controller.get
 );
 
@@ -41,15 +41,29 @@ router.get(
  *         description: Ok
  *         examples:
  *           application/json:
- *             { data:
-                  [ { id: 1,
-                      platform: 'ETH',
-                      token_name: 'INFT',
-                      token_symbol: 'INFT',
-                      token_address: '0x1716a6f9D3917966d934Ce7837113A30dFFda9F4',
-                      actived_flg:: true,
-                      createdAt: '2020-01-13T03:15:23.525Z',
-                      updatedAt: '2020-01-13T03:15:23.525Z' } ] }
+ *             {
+                "data": {
+                    "items": [
+                        {
+                            "id": 3,
+                            "platform": "ETH",
+                            "token_name": "MYHN",
+                            "token_symbol": "ETH",
+                            "token_address": "0x1716a6f9D3917966d934Ce7837113A30dFFda9F4",
+                            "actived_flg": true,
+                            "created_by": 65,
+                            "updated_by": 0,
+                            "tx_id": null,
+                            "wait_blockchain_confirm_status_flg": false,
+                            "createdAt": "2020-03-09T09:23:50.627Z",
+                            "updatedAt": "2020-03-09T09:32:05.325Z"
+                        }
+                    ],
+                    "offset": 0,
+                    "limit": 20,
+                    "total": 1
+                }
+            }
  *       400:
  *         description: Error
  *         schema:
@@ -72,7 +86,7 @@ router.put(
   '/payouts/:id',
   validator(update),
   authenticate,
-  authority(Permission.UPDATE_PAYOUT_STAKING_PLATFORM),
+  authority(Permission.UPDATE_PAYOUT_CONFIG),
   controller.update
 );
 
@@ -136,7 +150,7 @@ router.post(
   '/payouts',
   validator(create),
   authenticate,
-  authority(Permission.CREATE_PAYOUT_STAKING_PLATFORM),
+  authority(Permission.CREATE_PAYOUT_CONFIG),
   controller.create
 );
 
