328a8c00129ea4afc30008af60c301d8effd5903
trinhdn <trinhdn@blockchainlabs.asia>
2020-03-11 11:04:54 +0700

    update staking platforms

--- app/config/index.js
+++ app/config/index.js
@@ -77,6 +77,144 @@ const config = {
   schedule: {
     checkTransaction: process.env.SCHEDULE_ERC20_CHECK_TRANSACTION
   },
+  insight: {
+    ATOM: {
+      server: process.env.MULTICHAIN,
+      api: [
+        {
+          name: 'getLatestBlock',
+          method: 'GET',
+          url: "/api/chains/v1/ATOM/sync"
+        },
+        {
+          name: 'getTransaction',
+          method: 'GET',
+          url: "/api/chains/v1/ATOM/tx/{txId}",
+          params: ['txId'],
+        },
+        {
+          name: 'getDistributionOfValidator',
+          method: 'GET',
+          url: "/api/chains/v1/ATOM/distribution/validators/{validatorAddr}",
+          params: ['validatorAddr'],
+        },
+        {
+          name: "withdrawRewardsOfValidator",
+          method: "POST",
+          url: "/api/chains/v1/ATOM/distribution/validators/{validatorAddr}/rewards",
+          params: [
+            "validatorAddr"
+          ]
+        }
+
+      ],
+    },
+    IRIS: {
+      server: process.env.MULTICHAIN,
+      api: [
+        {
+          name: 'getLatestBlock',
+          method: 'GET',
+          url: "/api/chains/v1/IRIS/sync"
+        },
+        {
+          name: 'getTransaction',
+          method: 'GET',
+          url: "/api/chains/v1/IRIS/tx/{txId}",
+          params: ['txId'],
+        },
+        {
+          name: 'getDistributionOfValidator',
+          method: 'GET',
+          url: "/api/chains/v1/IRIS/distribution/{address}/rewards",
+          params: ['address'],
+        }
+      ],
+    },
+    TEZOS: {
+      server: process.env.MULTICHAIN,
+      api: [
+        {
+          name: 'getLatestBlock',
+          method: 'GET',
+          url: "/api/chains/v1/TEZOS/block/latest"
+        },
+        {
+          name: 'getCounter',
+          method: 'GET',
+          url: "/api/chains/v1/TEZOS/addr/{address}/counter",
+          params: ['address']
+        },
+        {
+          name: 'sendTransaction',
+          method: 'POST',
+          url: "/api/chains/v1/TEZOS/tx/send",
+          params: [],
+        },
+      ],
+    }
+  },
+  txCreator: {
+    host: process.env.TX_CREATOR_HOST,
+    ATOM: {
+      keyId: process.env.TX_CREATOR_ATOM_KEY_ID,
+      serviceId: process.env.TX_CREATOR_ATOM_SERVICE_ID,
+      index: process.env.TX_CREATOR_ATOM_INDEX,
+      testNet: process.env.TX_CREATOR_ATOM_TESTNET,
+      feeDenom: process.env.TX_CREATOR_ATOM_FEEDENOM || 'uatom',
+      gas: process.env.TX_CREATOR_ATOM_GAS_LIMIT ? parseInt(process.env.TX_CREATOR_ATOM_GAS_LIMIT) : 250000
+    },
+    IRIS: {
+      keyId: process.env.TX_CREATOR_IRIS_KEY_ID,
+      serviceId: process.env.TX_CREATOR_IRIS_SERVICE_ID,
+      index: process.env.TX_CREATOR_IRIS_INDEX,
+      testNet: process.env.TX_CREATOR_IRIS_TESTNET,
+      feeDenom: process.env.TX_CREATOR_IRIS_FEEDENOM || 'iris-atto',
+      gas: process.env.TX_CREATOR_IRIS_GAS_LIMIT ? parseInt(process.env.TX_CREATOR_IRIS_GAS_LIMIT) : 50000,
+      rateGasPrice: process.env.TX_CREATOR_IRIS_RATE_GAS_PRICE ? parseInt(process.env.TX_CREATOR_IRIS_RATE_GAS_PRICE) : 1e9
+    },
+    XTZ: {
+      keyId: process.env.TX_CREATOR_TEZOS_KEY_ID,
+      serviceId: process.env.TX_CREATOR_TEZOS_SERVICE_ID,
+      index: process.env.TX_CREATOR_TEZOS_INDEX,  
+      testNet: process.env.TX_CREATOR_TEZOS_TESTNET,
+      fee: process.env.TX_CREATOR_TEZOS_FEE
+    },
+    ETH: {
+      keyId: process.env.ERC20_TX_CREATOR_KEY_ID,
+      serviceId: process.env.ERC20_TX_CREATOR_SERVICE_ID,
+      index: process.env.ERC20_TX_CREATOR_INDEX,
+      testNet: process.env.ERC20_TX_CREATOR_TESTNET,
+      fee: process.env.ERC20_ETH_GAS_PRICE,
+      gasLimit: process.env.ERC20_ETH_GAS_LIMIT
+    }
+  },
+  schedule: {
+    cosmos: {
+      withdrawal: process.env.SCHEDULE_COSMOS_WITHDRAWAL,
+      distribution: process.env.SCHEDULE_COSMOS_DISTRIBUTION,
+      checkTransaction: process.env.SCHEDULE_COSMOS_CHECKTRANSACTION
+    },
+    iris: {
+      withdrawal: process.env.SCHEDULE_IRIS_WITHDRAWAL,
+      distribution: process.env.SCHEDULE_IRIS_DISTRIBUTION,
+      checkTransaction: process.env.SCHEDULE_IRIS_CHECKTRANSACTION
+    },
+    tezos: {
+      distribution: process.env.SCHEDULE_TEZOS_DISTRIBUTION,
+      checkTransaction: process.env.SCHEDULE_TEZOS_CHECKTRANSACTION
+    },
+    erc20: {
+      distributionReward: process.env.SCHEDULE_ERC20_REWARD_DISTRIBUTION,
+      distributionCommission: process.env.SCHEDULE_ERC20_COMMISSION_DISTRIBUTION,
+      checkTransaction: process.env.SCHEDULE_ERC20_CHECKTRANSACTION
+    }
+  },
+  sdk: {
+    apiKey: process.env.SDK_API_KEY,
+    secret: process.env.SDK_SECRET_KEY,
+    url: process.env.SDK_URL
+  }
 };
 
 module.exports = config;
