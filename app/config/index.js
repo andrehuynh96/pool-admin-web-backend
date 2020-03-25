8dbcae0ac981ebb28b214ff987f2e178b0a214b9
Huy Hoang <huyht@blockchainlabs.asia>
2020-03-25 17:56:27 +0800

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -51,7 +51,7 @@ const config = {
   mailSendAs: process.env.MAIL_SEND_AS || 'no-reply@infinito.io',
   website: {
     url: process.env.WEBSITE_URL,
-    urlActive: process.env.WEBSITE_URL + '/login?token=',
+    urlActive: process.env.WEBSITE_URL + '/set-new-password/',
     urlResetPassword: process.env.WEBSITE_URL + '/set-new-password/',
     urlConfirmNewIp: process.env.WEBSITE_URL + '/confirm-ip',
     urlImages: process.env.PARTNER_NAME ? process.env.WEBSITE_URL + '/' + process.env.PARTNER_NAME.toLowerCase() : process.env.WEBSITE_URL,
