bb5e2cf35e5722b9e2c68d92db636859c582b375
Unknown <huyht@blockchainlabs.asia>
2020-04-22 19:13:56 +0700

    fix bug commission
    fix bug commission

--- app/feature/response-schema/partner-commission.response-schema.js
+++ app/feature/response-schema/partner-commission.response-schema.js
@@ -4,20 +4,24 @@ const destObject = {
   single: {
     id: 'id',
     platform: 'platform',
+    symbol: 'symbol',
     commission: 'commission',
     reward_address: 'reward_address',
+    staking_platform_id: 'staking_platform_id',
     updated_by: 'updated_by',
     updatedAt: 'updated_at',
-    updated_by_user_name: 'updated_by_user_name'
+    partner_updated_by: 'partner_updated_by'
   },
   array: {
     '[].id': '[].id',
     '[].platform': '[].platform',
+    '[].symbol': '[].symbol',
     '[].commission': '[].commission',
     '[].reward_address': '[].reward_address',
+    '[].staking_platform_id': '[].staking_platform_id',
     '[].updated_by': '[].updated_by',
     '[].updatedAt': '[].updated_at',
-    '[].updated_by_user_name': '[].updated_by_user_name'
+    '[].partner_updated_by': '[].partner_updated_by'
   }
 };
 
