44768a5e241a55f365289038274c56fa40c3e795
Hung Tran <hungtv@blockchainlabs.asia>
2020-09-03 17:47:12 +0700

    Only return details of errror

--- app/middleware/validator.middleware.js
+++ app/middleware/validator.middleware.js
@@ -4,7 +4,9 @@ module.exports = function (schema) {
   return function (req, res, next) {
     const result = joi.validate(req.body, schema);
     if (result.error) {
-      return res.badRequest(res.__('MISSING_PARAMETERS'), 'MISSING_PARAMETERS', result.error);
+      return res.badRequest(res.__('MISSING_PARAMETERS'), 'MISSING_PARAMETERS', {
+        details: result.error.details,
+      });
     }
 
     next();
